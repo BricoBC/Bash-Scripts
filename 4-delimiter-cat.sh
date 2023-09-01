53d45110591d19d0544c1b42c3423129727f6e6d
BricoBC <barajas.c.bruno.n@gmail.com>
2023-09-01 13:21:41 -0600

    add file delimiter

---
+++ 4-delimiter-cat.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+: 'La función de cat nos permite mostrar el texto
+El texto que va a mostrar tienen que tener el principio y al final su identificador
+Ejemplo:
+cat << identificador
+Este es un texto
+de ejemplo
+identificador
+'
+
+cat << myText
+Es un texto
+que se va a imprimir
+en la terminal
+myText
