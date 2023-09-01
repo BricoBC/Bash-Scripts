bcaf078040055a1e9cfc763cfc1d05272e8f5bd0
BricoBC <barajas.c.bruno.n@gmail.com>
2023-09-01 13:01:29 -0600

    Add save output with > and >>

---
+++ 2-save-output.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+#Espera una entrada de texto y el texto ingresado se almacena en un archivo txt
+cat > saludo.txt
---
+++ 3-save-output2.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+
+#Almacena en el archivo saludo.txt todo lo que vayamos escribiendo en todas las ejecuciones
+cat >> saludo2,0.txt
