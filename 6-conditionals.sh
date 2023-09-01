74abbf47fc7a9af4fe620998857d47d97edb03e3
BricoBC <barajas.c.bruno.n@gmail.com>
2023-09-01 14:15:18 -0600

    Add file of conditionals

---
+++ 6-conditionals.sh
@@ -0,0 +1,31 @@
+#! /bin/bash
+
+# -eq == 
+# -ge >= (greater equal)
+# -le <= (less equal)
+
+#Una variable que se iguala a 21
+#Nota: Es importante que no tenga espacios
+age=17
+
+#if [ conditional ]
+#Nota: Es importante los espacios con los []
+if [ $age -ge 18 ]
+then
+    echo "Eres mayor de edad"
+# elif [ $age -eq 17 ]
+# then
+#     echo "Casi eres un adulto"
+else
+    echo "No eres mayor de edad"
+fi
+
+#Otra forma para escribir el condicional es el siguiente:
+: '
+if (( age >= 18 ))
+then
+    echo "Eres mayor de edad"
+else
+    echo "No eres mayor de edad"
+fi
+'
