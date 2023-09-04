e7dffd67c5cd50c7f77383c54ac6dea4fd37804f
BricoBC <barajas.c.bruno.n@gmail.com>
2023-09-04 13:40:12 -0600

    Add file to concatenate

---
+++ 14-concatenate.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+echo "Introduce un nombre: "
+read name
+
+echo "Introduce un adejtivo: "
+read adjective
+
+result="$name es $adjective"
+echo $result
+
+#Minusculas
+#echo ${name,,}
+#Mayusculas
+#echo ${name^^}
+
+#Indicamos las letras en los corchetes para que lo pase a mayusculas
+echo ${name^^[aeiou]}
+
+#Indicamos las letras en los corchetes para que lo pase a minusculas
+echo ${name,,[AEIOU]}
+
