fe9ece8944423e85f490dfa725cea47d1a9285c6
BricoBC <barajas.c.bruno.n@gmail.com>
2023-09-04 14:37:30 -0600

    modify delete the read and echo to send a parameter (file.sh)

--- P1.sh
+++ P1.sh
@@ -1,7 +1,12 @@
 #! /bin/bash
 
+#Primera versión
 #Imprimimos una cadena de texto
-echo "Ingresa el nombre de tu archivo: "
-read fichero
-chmod +x $fichero
-source $fichero
+# echo "Ingresa el nombre de tu archivo: "
+# read fichero
+# chmod +x $fichero
+# source $fichero
+
+#Segunda versión
+chmod +x $@
+source $1
